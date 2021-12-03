07a3fdafd0edbe23fc3e22c1e40d56b5a33fba54
weorul12 <90319460+weorul12@users.noreply.github.com>
2021-12-03 14:49:03 +0300

    Add files via upload

---
+++ Лучший код/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] R = new int[25];
+Random Rand = new Random();
+    Console.WriteLine("До:");
+for(int i =0; i<R.Length; i++)
+   {R[i] = Rand.Next(-9,9); Console.WriteLine("Эл массива {0}", R[i]);}
+    Console.WriteLine("После:");
+for(int i=0; i<R.Length; i++)
+{
+   if(R[i]<0)
+      R[i]=i*i;
+   else
+      if(R[i]>0)
+         R[i]=i+7;
+ Console.WriteLine("Эл массива {0}", R[i]);
+}
