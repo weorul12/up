9b1a99960982d10a8bb90de6ecce8fa4622345f9
weorul12 <90319460+weorul12@users.noreply.github.com>
2021-12-04 09:22:21 +0300

    Add files via upload

---
+++ Лучший код 2.0/Program.cs
@@ -0,0 +1,16 @@
+﻿ 
+double getResult(int n,int x){
+    return n/(Math.Pow(x,n));
+}
+
+Console.WriteLine("Введите X:");
+int x=Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int n=Int32.Parse(Console.ReadLine());
+
+double c=0;
+for (int i=1; i<=n; i++)
+{
+c=c+getResult(i,x);
+}
+Console.WriteLine("Ответ:{0}",c);
